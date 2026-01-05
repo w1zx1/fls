940ba83878b049a55875d91f4c23f602e88c9822
w1zx1 <kirinsasa01@yandex.ru>
2026-01-05 14:17:10 +0300

    make motd changeable in config.yml

--- LanSpammer.java
+++ LanSpammer.java
@@ -10,6 +10,7 @@ public class LanSpammer {
 
         String yourIp;
         int servers;
+        String motdBase;
 
         try (InputStream in = new FileInputStream("config.yml")) {
             Yaml yaml = new Yaml();
@@ -17,6 +18,7 @@ public class LanSpammer {
 
             yourIp = (String) config.get("ip");
             servers = ((Number) config.get("servers")).intValue();
+            motdBase = (String) config.get("motd");
         } catch (Exception e) {
             throw new RuntimeException("cant read config.yml", e);
         }
@@ -28,18 +30,20 @@ public class LanSpammer {
             InetAddress localAddr = InetAddress.getByName(yourIp);
             InetAddress group = InetAddress.getByName(multicastAddr);
             
-            MulticastSocket socket = new MulticastSocket(new InetSocketAddress(localAddr, 0));
+            MulticastSocket socket =
+                    new MulticastSocket(new InetSocketAddress(localAddr, 0));
             socket.setTimeToLive(4);
 
             System.out.println("Started on Ip: " + yourIp);
 
             while (true) {
                 for (int i = 1; i <= servers; i++) {
-                    // MOTD
-                    String motd = "Test " + i;
-                    // PORT
+
+                    String motd = motdBase + i;
                     int serverPort = 25560 + i;
-                    String message = "[MOTD]" + motd + "[/MOTD][AD]" + serverPort + "[/AD]";
+
+                    String message =
+                            "[MOTD]" + motd + "[/MOTD][AD]" + serverPort + "[/AD]";
                     
                     byte[] buffer = message.getBytes(StandardCharsets.UTF_8);
                     DatagramPacket packet =
